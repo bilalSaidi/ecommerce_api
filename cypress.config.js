9e9fb96c7a424ddb1c66f8450906c372d313e498
bilalsaidi <bilal.saidibatna@gmail.com>
2022-07-21 16:52:27 +0200

    adding junit report

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,11 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  reporter: 'junit',
+  reporterOptions: {
+    mochaFile: 'results/res-[hash].xml',
+    toConsole: true
+  },
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
